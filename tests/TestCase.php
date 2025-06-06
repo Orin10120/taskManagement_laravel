7c24c33af57251d028d43b9864545ba877592707
Rindang Bani Isyan <109974656+Orin10120@users.noreply.github.com>
2025-06-06 21:34:45 +0700

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use CreatesApplication;
 
     protected function setUp(): void
     {
